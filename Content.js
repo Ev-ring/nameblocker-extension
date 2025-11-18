a3da7515eeff16b7708366c64ddfb70d883e400b
Ev-ring <127316305+Ev-ring@users.noreply.github.com>
2025-11-18 11:55:35 -0500

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,1 +1,10 @@
-# blah blah blah
+//rahhh
+
+const visibletext = document.body.innerText;
+const nameList = fetch('https://github.com/names.json')
+
+
+for (txt of visibletext){
+    if (txt == 
+}
+  
